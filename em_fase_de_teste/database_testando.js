3ce9110cceabb806ec7ade0ae704b8779b181b5a
Thiago de Almeida Pereira <thiago.almeida@proponto.com.br>
2023-03-23 00:19:46 -0300

    melhorias

--- em_fase_de_teste/database_testando.js
+++ em_fase_de_teste/database_testando.js
@@ -1,34 +1,90 @@
 // get the client
 const mysql = require('mysql2');
+const readXLS = require('./../read_xls');
+
+const file = './../teste.xls';
+
+const xls = readXLS(file);
+
+// Obtendo o nome das colunas
+
+const columnNames = Object.keys(xls[0]);
+
+//console.log(columnNames);
+
+// Nome da tabela, a tabela precisa existirs
+
+const tableName = 'contatos';
+
+// Preparando o comando SQL
+
+function insert(tableName, json) {
+
+  let sql = 'insert into ' + tableName + ' (';
+
+  sqlColumnNames = columnNames.join(", ");
+
+  sql += sqlColumnNames + ' ) ';
+
+  let values = Object.values(json); // retorna um array com os valores do jsons
+
+  let sqlValues = ' values ( ';
+
+  for (let index = 0; index < values.length; index++) {
+
+    if(typeof(values[index]) == 'string')    {
+      
+      sqlValues += ' `'+ values[index] + '`';
+
+    }else {
+      sqlValues += ' ' + values[index] + '';
+    }
+
+    if(index < (values.length - 1)) {
+
+      sqlValues += ', ';
+
+    }
+
+  }  
+
+  sql += sqlValues + ' )';
+
+  console.log(sql);
+
+  return sql;
+
+}
+
+insert(tableName, xls[0]);
+
+
+
 
 // create the connection to database
+
 /*
+
 const connection = mysql.createConnection({
   host: '127.0.0.1',
-  port: 3306,
   user: 'root',
-  password: '',
-  database: ''
+  password: 'dev',
+  database: 'proponto',
+  port: 13306
 });
+
+
+connection.query('SELECT id, document, email FROM account LIMIT 5', (err, res) => { 
+
+  for (let index = 0; index < xls.length; index++) {
+
+    // insere os resultados
+    
+  }
+
+  connection.end(); 
+
+});
+
 */
 
-// exemplo de connection string: mysql://root:password@localhost:port/dbName
-
-const connection = mysql.createConnection("mysql://root:password@localhost:port/dbName");
-
-
-// connection.connect(function (err) {
-
-//     console.log("Conexão com o banco de dados realizada com sucesso.");
-
-// });
-
-
-const sql = "select * from account limit 10";
-
-const results = connection.query('SELECT * FROM account LIMIT 5', (err, rows, fields) => {
-  return rows;
-});
-
-
-console.log(results);
